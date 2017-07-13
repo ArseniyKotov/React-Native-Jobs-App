cad258ce0c5b60a4026b28ab77583c88dda81afa
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-07-13 09:07:09 -0700

    adding mapview

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -3,7 +3,6 @@ import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 
-
 class AuthScreen extends Component {
   componentDidMount() {
     this.props.facebookLogin();
--- screens/Slides.js
+++ screens/Slides.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 
 class Slides extends Component {
   render() {
     return (
-      <div />
+      <View />
     );
   }
 }
